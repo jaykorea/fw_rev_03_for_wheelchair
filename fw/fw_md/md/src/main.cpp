4b2263d22ee57ecae00782d1b346bd10965c96b2
jaykorea <jhjo1997@naver.com>
2023-05-10 13:58:30 +0900

    convert to timer callback to call super emer function

--- fw/fw_md/md/src/main.cpp
+++ fw/fw_md/md/src/main.cpp
@@ -11,9 +11,12 @@
 #include "md/monitor_msg.h"
 
 #include <ros/ros.h>
+//#include <thread>
 #include <boost/bind.hpp>
+#include <std_msgs/Empty.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int8.h>
 #include <math.h>
 #include <sstream>
 #include "geometry_msgs/Twist.h"
@@ -31,9 +34,18 @@ bool once_flag = false;
 bool pre_release_flag = false;
 bool front_obstacle_detected = false;
 float ttc_time;
-freeway_msgs::DistanceTimeCalculator dt;
+freeway_msgs::DistanceTimeCalculator dt_msg;
 geometry_msgs::Twist cmd_vel_msg;
-nav_msgs::Odometry odom;
+nav_msgs::Odometry odom_msg;
+std_msgs::Int8 resume_feedback_msg;
+
+ros::Publisher cmd_emer_super_pub;
+ros::Publisher cancel_pub;
+ros::Publisher resume_pub;
+ros::ServiceClient clearcostmap_pub;
+
+ros::Timer e_stop_timer;
+ros::Timer resume_timer;
 
 TableOfRobotControlRegister RB;
 Communication Com;
@@ -68,6 +80,102 @@ void cmd_vel_update_cb(const geometry_msgs::Twist::ConstPtr& msg) {
     cmd_vel_msg = *msg;
 }
 
+void odom_msg_update_cb(const nav_msgs::Odometry::ConstPtr& msg) {
+    odom_msg = *msg;
+}
+
+void dt_update_cb(const freeway_msgs::DistanceTimeCalculator::ConstPtr& msg) {
+    dt_msg = *msg;
+}
+
+void resume_feedback_update_cb(const std_msgs::Int8::ConstPtr& msg) {
+    resume_feedback_msg = *msg;
+}
+//
+//void e_stop_thread_function()
+//{
+//    ros::Rate r(40); // Set the loop rate to 40 Hz
+//    while (ros::ok())
+//    {
+//        // Your code here
+//        if ( e_stop_flag == false && front_obstacle_detected == true) {
+//            // Stop the robot if a front obstacle is detected
+//            geometry_msgs::Twist zero_velocity;
+//            zero_velocity = geometry_msgs::Twist();
+//            actionlib_msgs::GoalID empty_goal;
+//            cancel_pub.publish(empty_goal);
+//            InitSetBrakeStop();
+//	    int loop_count = 0;
+//            do {
+//		cmd_emer_super_pub.publish(zero_velocity);
+//		r.sleep();
+//           } while (cmd_vel_msg.linear.x > 0.0 || odom_msg.twist.twist.linear.x > 0.0);
+//            e_stop_flag = true;
+//        }
+//        else if (e_stop_flag == true && front_obstacle_detected == false) {
+//            // Resume normal operation if no front obstacle is detected
+//            e_stop_flag = false;
+//        }
+//
+//        ros::spinOnce();
+//        r.sleep();
+//    }
+//}
+//
+//
+//void e_stop_tim_cb(const ros::TimerEvent& evnet, ros::Timer& timer) {
+//    geometry_msgs::Twist zero_velocity;
+//    zero_velocity = geometry_msgs::Twist();
+//    actionlib_msgs::GoalID empty_goal;
+//    if(dt_msg.status_info == 1) cancel_pub.publish(empty_goal);
+//
+//    if (cmd_vel_msg.linear.x != 0.0 || odom_msg.twist.twist.linear.x != 0.0 ) {
+//       cmd_emer_super_pub.publish(zero_velocity);
+//     }
+//      else {
+//        timer.stop();
+//     }
+//}
+
+void e_stop_timer_callback(const ros::TimerEvent& event, ros::NodeHandle& nh)
+{
+    if ( e_stop_flag == false && front_obstacle_detected == true) {
+        e_stop_flag = true;
+        geometry_msgs::Twist zero_velocity;
+        zero_velocity = geometry_msgs::Twist();
+        actionlib_msgs::GoalID empty_goal;
+        if(dt_msg.status_info == 1) cancel_pub.publish(empty_goal);
+//        InitSetBrakeStop();
+//	e_stop_timer = nh.createTimer(ros::Duration(0.025), boost::bind(&e_stop_tim_cb, _1,  boost::ref(e_stop_timer)));
+        // Start a timer to check if cmd_vel_msg.linear.x decreases to zero
+        e_stop_timer = nh.createTimer(ros::Duration(0.025), [&](const ros::TimerEvent&){
+            if (cmd_vel_msg.linear.x > 0.0 || odom_msg.twist.twist.linear.x > 0.0) {
+              cmd_emer_super_pub.publish(zero_velocity);
+            }
+            else {
+        		InitSetBrakeStop();
+                e_stop_timer.stop();
+            }
+        });
+    }
+    else if (e_stop_flag == true && front_obstacle_detected == false) {
+         // Resume normal operation if no front obstacle is detected
+          std_srvs::Empty srv;
+          clearcostmap_pub.call(srv);
+          e_stop_flag = false;
+          resume_timer = nh.createTimer(ros::Duration(4.0), [&](const ros::TimerEvent&){
+            ROS_INFO("Resume Super timer callback Started!!!");
+            std_msgs::Empty resume_msg;
+            resume_pub.publish(resume_msg);
+            bool resume_feedback_msg_data = resume_feedback_msg.data;
+            if (resume_feedback_msg_data == -1) {
+                ROS_INFO("Resume Super timer callback Stopped!!!, status_info: %d", resume_feedback_msg.data);
+                resume_timer.stop();
+               }
+          });
+    }
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "md_node");                                                   //Node name initialization.
@@ -75,13 +183,26 @@ int main(int argc, char** argv)
     ros::Subscriber vel_sub = nh.subscribe("vel_topic", 100, velCallBack);
     ros::Subscriber release_button_sub = nh.subscribe("/freeway/release", 10, release_cb);
     ros::Subscriber front_obstacle_sub = nh.subscribe("/freeway/front_obstacle_teb", 1, front_obstacle_update_cb);
-    ros::Subscriber dt_sub = nh.subscribe("/freeway/initsetbrakestop", 10, init_set_brake_stop_cb);
+    ros::Subscriber e_stop__sub = nh.subscribe("/freeway/initsetbrakestop", 10, init_set_brake_stop_cb);
+    ros::Subscriber dt_sub = nh.subscribe("/freeway/distancetimecalculator", 10, dt_update_cb);
     ros::Subscriber cmd_sub = nh.subscribe("/cmd_vel", 10, cmd_vel_update_cb);
-    ros::Publisher cancel_pub = nh.advertise<actionlib_msgs::GoalID>("move_base_flex/move_base/cancel", 10);
+    ros::Subscriber odom_sub = nh.subscribe("/odom_md", 10, odom_msg_update_cb);
+    ros::Subscriber resume_feedback_sub = nh.subscribe("/freeway/resume/feedback", 10, resume_feedback_update_cb);
+    cancel_pub = nh.advertise<actionlib_msgs::GoalID>("move_base_flex/move_base/cancel", 10);
     ros::Publisher monitor_pub = nh.advertise<md::monitor_msg>("monitor_topic", 100);   //Publisher declaration.
     ros::Publisher string_pub = nh.advertise<std_msgs::String>("string_com_topic", 100);
-    ros::Publisher cmd_emer_super_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel/super_emer", 1);
+    cmd_emer_super_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel/super_emer", 1);
+    clearcostmap_pub = nh.serviceClient<std_srvs::Empty>("move_base_flex/clear_costmaps");
+    resume_pub = nh.advertise<std_msgs::Empty>("/freeway/resume", 10);
+
     md::monitor_msg monitor;                                                            //monitor_msg declares message 'message' as message file.
+
+    //std::thread e_stop_thread(e_stop_thread_function);
+    // create a timer and pass the node handle to the callback function
+    //ros::Timer timer = nh.createTimer(ros::Duration(0.025), e_stop_timer_callback(event, nh);
+    ros::Timer timer = nh.createTimer(ros::Duration(0.025), [&](const ros::TimerEvent& event){
+        e_stop_timer_callback(event, nh);
+    });
 
     ros::Rate r(10000);                                                                 //Set the loop period -> 100us.
 
@@ -362,21 +483,21 @@ int main(int argc, char** argv)
                 }
             }
         }
-        if ( e_stop_flag == false && front_obstacle_detected == true) {
+    //    if ( e_stop_flag == false && front_obstacle_detected == true) {
         // Stop the robot if a front obstacle is detected
-        geometry_msgs::Twist zero_velocity;
-	zero_velocity = geometry_msgs::Twist();
-	actionlib_msgs::GoalID empty_goal;
-	cancel_pub.publish(empty_goal);
-        InitSetBrakeStop();
-	do { cmd_emer_super_pub.publish(zero_velocity);
-        } while (cmd_vel_msg.linear.x == 0.0);
-	e_stop_flag = true;
-        } 
-        else if (e_stop_flag == true && front_obstacle_detected == false) {
+  //      geometry_msgs::Twist zero_velocity;
+//	zero_velocity = geometry_msgs::Twist();
+//	actionlib_msgs::GoalID empty_goal;
+//	cancel_pub.publish(empty_goal);
+  //      InitSetBrakeStop();
+//	do { cmd_emer_super_pub.publish(zero_velocity);
+       // } while (cmd_vel_msg.linear.x == 0.0);
+       //e_stop_flag = true;
+       // } 
+       // else if (e_stop_flag == true && front_obstacle_detected == false) {
           // Resume normal operation if no front obstacle is detected
-          e_stop_flag = false;
-        }
+       //   e_stop_flag = false;
+       // }
         // if(SET_STOP_STATUS) {
         //     if( pre_release_flag != release_flag) once_flag = true;
         //     if (release_flag && once_flag) { //release button pushed
